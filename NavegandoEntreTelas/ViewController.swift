19ef67b65eeb98b6c46f993dd1735f6c64cbe9c3
Gabriel P <gpapa.contato@gmail.com>
2020-06-09 23:09:23 -0700

    Navigation

---
+++ NavegandoEntreTelas/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NavegandoEntreTelas
+//
+//  Created by Agesandro Scarpioni on 12/05/20.
+//  Copyright © 2020 Agesandro Scarpioni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func fechar(_sender: Any){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
+
